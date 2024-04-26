f81b05116e29c539339ed3b38f9be3ffdd70acfa
Надежда Шутько <shutko_bstu@mail.ru>
2024-04-26 21:25:33 +0300

    Настроит хэширование

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,22 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/main.js', // Точка входа
   output: {
-    filename: 'bundle.js', // Имя бандла
+    filename: 'bundle.[contenthash].js', // Имя бандла
     path: path.resolve(__dirname, 'build'), // Директория для файлов сборки
     clean: true, // Удаляем предыдущую сборку перед созданием новой
   },
   devtool: 'source-map', // Генерируем карту исходного кода
   plugins: [ // Подключаем плагины
-    // new HtmlPlugin({
-    //   template: 'public/index.html',
-    // }),
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
       patterns: [
         {
-          from: 'public'
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
         },
       ],
     }),
