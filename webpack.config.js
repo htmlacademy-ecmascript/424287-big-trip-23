abc22c845cfb9a3f93ea871d5cdf0ac6ae45b783
Надежда Шутько <shutko_bstu@mail.ru>
2024-04-26 21:39:55 +0300

    Подключит HtmlPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-// const HtmlPlugin = require('html-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js', // Точка входа
@@ -36,11 +36,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
+      }
     ],
   },
 };
