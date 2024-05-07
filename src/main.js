823b368202c9b6df85a7c8f3eea73503bf3157e4
Надежда Шутько <shutko_bstu@mail.ru>
2024-05-07 16:57:22 +0300

    Создаст экзмепляр модели

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import GeneralPresenter from './presenter/general-presenter';
+import PointModel from './model/point-model';
+
 const tripControlsFilters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
-const generalPresenter = new GeneralPresenter({tripControlsFilters:tripControlsFilters,tripEvents:tripEvents});
+const pointModel = new PointModel();
+const generalPresenter = new GeneralPresenter({tripControlsFilters:tripControlsFilters,tripEvents:tripEvents,pointModel});
 generalPresenter.init();
