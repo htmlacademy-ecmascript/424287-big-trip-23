728fc5db772fe0bc72b75138fd4d379adba3455d
Надежда Шутько <shutko_bstu@mail.ru>
2024-05-10 16:33:48 +0300

    Изменит презентер и вью

--- src/presenter/general-presenter.js
+++ src/presenter/general-presenter.js
@@ -4,7 +4,6 @@ import WayPoint from '../view/way-point.js';
 import EditingForm from '../view/editing-form.js';
 import EventListView from '../view/event-list-view.js';
 import {RenderPosition, render} from '../render.js';
-
 export default class GeneralPresenter {
   eventListComponent = new EventListView();
   constructor({tripControlsFilters,tripEvents,pointModel}) {
@@ -14,16 +13,17 @@ export default class GeneralPresenter {
   }
 
   init() {
-    this.boardPoints = [...this.pointModel.getEvents()];
-    this.destination = this.pointModel.getDestination();
-    console.log(this.destination);
-
+    const events = [...this.pointModel.getEvents()];
+    const destinations = [...this.pointModel.getDestinations()];
+    const offers = [...this.pointModel.getOffers()];
     render(new FiltersView(),this.tripControlsFilters,RenderPosition.BEFOREEND);
     render(new SortView(),this.tripEvents);
     render(this.eventListComponent,this.tripEvents);
-    render(new EditingForm(),this.eventListComponent.getElement());
-    for(let i = 0; i < this.boardPoints.length; i++) {
-      render(new WayPoint({event: this.boardPoints[i],destinations:this.destination[i]}),this.eventListComponent.getElement());
-    }
+    render(new EditingForm({destinations, offers}),this.eventListComponent.getElement());
+    render(new EditingForm({event,destinations, offers}),this.eventListComponent.getElement());
+    events.forEach((event) => {
+      render(new WayPoint({event,destinations, offers}),this.eventListComponent.getElement());
+    });
   }
 }
+
--- src/view/way-point.js
+++ src/view/way-point.js
@@ -1,38 +1,47 @@
-// import { destination } from '../mock/cities';
 import { createElement } from '../render';
-import { humanizeDueDate,humanizeDueTime, } from '../util';
-const createWayPointTemplate = (event,destinations) => {
+import dayjs from 'dayjs';
+
+import { humanizeDueDate,humanizeDueTime, machineDueFormat} from '../util';
+const createWayPointTemplate = (event,destinations,offers) => {
   const {type,dateFrom,dateTo,basePrice,isFavorite} = event;
-  // const currentDestination = destinations.find((destination) => destination.id === event.destination);
+  const currentDestination = destinations.find((destination) => destination.id === event.destination);
+  const typeOffers = offers.find((offer) => offer.type === event.type).offers;
+  const eventOffers = typeOffers.filter((typeOffer) => event.offers.includes(typeOffer.id));
   const date = humanizeDueDate(dateFrom);
   const timeStart = humanizeDueTime(dateFrom);
   const timeEnd = humanizeDueTime(dateTo);
-  // const tripTime = tripDuration(dateFrom,dateTo);
+  const date1 = dayjs(dateTo);
+  const tripDuration = date1.diff(dateFrom, 'm');
+
+  //   var a = dayjs(timeEnd)
+
+  // dayjs(timeStart).to(a)
+  //   console.log(dayjs(timeStart).to(a));
   return `<li class="trip-events__item">
   <div class="event">
-                <time class="event__date" datetime="2019-03-18">${date}</time>
+                <time class="event__date" datetime="${machineDueFormat(date)}">${date}</time>
                 <div class="event__type">
                   <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">${type} ${destinations.name}</h3>
+                <h3 class="event__title">${type} ${currentDestination.name}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
                     <time class="event__start-time" datetime="${dateFrom}">${timeStart}</time>
                     &mdash;
                     <time class="event__end-time" datetime="${dateTo}">${timeEnd}</time>
                   </p>
-                  <p class="event__duration">M</p>
+                  <p class="event__duration">${tripDuration}M</p>
                 </div>
                 <p class="event__price">
                   &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
                 </p>
                 <h4 class="visually-hidden">Offers:</h4>
                 <ul class="event__selected-offers">
-                  <li class="event__offer">
-                    <span class="event__offer-title">Order Uber</span>
-                    &plus;&euro;&nbsp;
-                    <span class="event__offer-price">20</span>
-                  </li>
+                  ${eventOffers.map((offer) => (`<li class="event__offer">
+                  <span class="event__offer-title">${offer.title}</span>
+                  &plus;&euro;&nbsp;
+                  <span class="event__offer-price">${offer.price}</span>
+                </li>`)).join('')}
                 </ul>
                 <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
                   <span class="visually-hidden">Add to favorite</span>
@@ -47,13 +56,14 @@ const createWayPointTemplate = (event,destinations) => {
 </li>`;
 };
 export default class WayPoint {
-  constructor({event, destinations}) {
+  constructor({event, destinations,offers}) {
     this.event = event;
     this.destinations = destinations;
+    this.offers = offers;
   }
 
   getTemplate() {
-    return createWayPointTemplate(this.event,this.destinations);
+    return createWayPointTemplate(this.event,this.destinations, this.offers);
   }
 
   getElement() {
